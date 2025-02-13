7a59ba57db5f3c550d51f37b41c642f7442115ed
justhal <justhal@gmail.com>
2025-02-13 14:27:34 -0500

    Added boot time as an example integrating git and GitHub into VS Code.

--- psutil-test.py
+++ psutil-test.py
@@ -7,3 +7,6 @@ print(psutil.cpu_percent(interval=1, percpu=True))
 
 print("Virtual memory:")
 print(psutil.virtual_memory())
+
+print("Boot time (seconds since the epoch):")
+print(psutil.boot_time())
